6c95bb709bd481cbce4243c771363fa43e680758
Alex Hoskins <alexander.hoskins@yahoo.com>
2021-12-03 13:08:12 -0800

    halfway done with mvp

--- api/server.js
+++ api/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const server = express();
-
+server.use(express.json());
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+const projectRouter = require('../api/projects/projects-router')
+server.use('/api/projects', projectRouter)
+
+const actionRouter = require('./actions/actions-router')
+server.use('/api/actions', actionRouter)
+
+
+// server.get('/', (req, res) => {
+//     res.send(`<h2>Let's write some middleware!</h2>`);
+//   });
 
 module.exports = server;
